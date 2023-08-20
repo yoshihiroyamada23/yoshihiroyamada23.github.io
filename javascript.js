ee3b7fe853e86650762c4ef8b7fbfe582a4c658c
Yoshihiro Yamada <118563876+yoshihiroyamada23@users.noreply.github.com>
2023-08-20 16:39:29 -0700

    Initial Commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+﻿var treasure = Math.floor(Math.random() * 8)
+var bomb = Math.floor(Math.random() * 8)
+var gamestatus = true
+
+while (treasure == bomb) {
+    bomb = Math.floor(Math.random() * 8)
+}
+const changeToEmoji = (number) => {
+    if (number == treasure && gamestatus) {
+        document.getElementById(number).innerHTML = "🪙"
+        alert ("You Win!")
+        gamestatus = false
+    } else if (number == bomb && gamestatus) {
+        document.getElementById(number).innerHTML = "💣"
+        alert("Game Over")
+        gamestatus = false
+    } else if (gamestatus) {
+        document.getElementById(number).innerHTML = "🍌"
+        
+        
+    }
+   
+}
